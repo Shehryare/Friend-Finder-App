b4d2855c525f5fb6ac8508333e7726a7e74c2282
Shehryar Ehtesham <se318@rutgers.edu>
2019-11-25 17:43:37 -0500

    finalized application

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+    {
+        name: "John 117",
+        photoURL: "https://upload.wikimedia.org/wikipedia/en/0/05/Master_Chief_in_Halo_5.png",
+        answers: ["5", "2", "2", "3", "1", "1", "5", "2", "5", "5"]
+    }, 
+    {
+        name: "Bruce Banner",
+        photoURL: "https://i.ytimg.com/vi/jolXso_OO-c/maxresdefault.jpg",
+        answers: ["2", "1", "2", "2", "1", "3", "3", "4", "1", "5"]
+    }, 
+    {
+        name: "Sheev Palpatine",
+        photoURL: "https://am21.akamaized.net/tms/cnt/uploads/2014/10/palpy.jpg",
+        answers: ["2", "1", "1", "4", "5", "4", "3", "3", "3", "3"]
+    }, 
+    {
+        name: "Tony",
+        photoURL: "https://vignette.wikia.nocookie.net/scarface/images/4/44/Tony_Montana.jpg/revision/latest?cb=20120604034628&path-prefix=en",
+        answers: ["3", "5", "4", "5", "2", "3", "4", "4", "3", "5"]
+    }, 
+    {
+        name: "Megan",
+        photoURL: "https://media.vanityfair.com/photos/5c0ef15ae2641b48c86501e9/2:3/w_888,h_1332,c_limit/Megan-Fox-MeToo-Story%20(1).jpg",
+        answers: ["1", "4", "2", "2", "3", "1", "4", "5", "5", "1"]
+    },
+    {
+        name: "Carol",
+        photoURL: "https://qph.fs.quoracdn.net/main-qimg-376073d305fc47222e5745660a8d2048",
+        answers: ["2", "4", "5", "3", "3", "5", "2", "5", "4", "2"]
+    },
+    {
+        name: "Arthur",
+        photoURL: "https://carboncostume.com/wordpress/wp-content/uploads/2018/10/arthurmorgan-reddeadredemption2.jpg",
+        answers: ["4", "5", "5", "3", "5", "5", "3", "5", "2", "3"]
+    },
+    {
+        name: "Argonian",
+        photoURL: "https://static.vectorplexus.com/uploads/screenshots/monthly_2016_08/Brutish-Argonian-Male.jpg.602cb3502379ac62d2aafc98b7b76616.jpg",
+        answers: ["1", "5", "4", "3", "3", "5", "2", "3", "5", "5"]
+    },
+    {
+        name: "Shahrukh",
+        photoURL: "https://media-images.mio.to/various_artists/K/King%20Khan%20-%20Shah%20Rukh%20Khan%20Hits%20%282017%29/Art-350.jpg",
+        answers: ["4", "5", "4", "4", "5", "1", "3", "2", "2", "5"]
+    },
+    {
+        name: "Elon",
+        photoURL: "https://image.businessinsider.com/5dd9a902fd9db212ca6834d3?width=1100&format=jpeg&auto=webp",
+        answers: ["2", "4", "2", "5", "4", "2", "5", "3", "1", "1"]
+    }
+];
+
+module.exports.friends = friends;
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// require("./app/routing/apiRoutes.js")(app);
-// require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function () {
     console.log("Sever is listening at http://localhost:" + PORT);
